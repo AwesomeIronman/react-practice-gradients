c55f07eae60fb42f743e458d68a18c86f07acf81
Cyrus Gracias <cyrusgracias4u@gmail.com>
2020-07-07 19:39:40 +0530

    use state and add styling

--- src/index.js
+++ src/index.js
@@ -7,9 +7,28 @@ import ColorList from "./components/ColorList.js";
 import GradientBox from "./components/GradientBox.js";
 
 function App() {
+	let [
+		color1,
+		setColor1
+	] = useState("red");
+	let [
+		color2,
+		setColor2
+	] = useState("green");
+
 	return (
 		<React.Fragment>
 			<h1 className="headline">CHOOSE YOUR COLORS</h1>
+
+			<div className="cta_labels">
+				<label for="color1">
+					<h2>Select first color:</h2>
+				</label>
+
+				<label for="color2">
+					<h2>Select second color:</h2>
+				</label>
+			</div>
 
 		</React.Fragment>
 	);
